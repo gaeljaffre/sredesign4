c638bf6539c1c3fd7c5b0c1a0fdead7b23b42bda
gaeljaffre <gael.jaffre@yahoo.fr>
2020-03-04 22:33:26 +0100

    1 file modified

--- src/app/config-bd.ts
+++ src/app/config-bd.ts
@@ -1,3 +1,4 @@
-export const urlBackend = "https://RedesignBackend.voyagesgael.repl.co/";
+//export const urlBackend = "https://RedesignBackend.voyagesgael.repl.co/";
+export const urlBackend = "https://redesignbackendfb1.voyagesgael.repl.co/";
 
 export const appliName = "Corsair Redesign (d)";
